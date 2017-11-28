8e3ee5495dc3c91edeaab26dc9d3dd8ca0637fe2
Rodrigo Dumont <rodrigo@rxdx.com.br>
2017-11-28 20:14:58 -0200

    Initial Commit

---
+++ iDog/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iDog
+//
+//  Created by Rodrigo Dumont on 28/11/17.
+//  Copyright © 2017 Rodrigo Dumont. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
